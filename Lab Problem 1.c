de824a6060cd0e93afc56ba7797ba0e293db87d0
Nowshin <105603725+Nowshin1009@users.noreply.github.com>
2022-05-20 00:01:04 +0600

    Update Lab Problem 1.c

--- Lab Problem 1.c
+++ Lab Problem 1.c
@@ -1,3 +1,6 @@
+/*.Niloys basic salary is input through the keyboard. His dearness allowance is 40% of his basic 
+salary and house rent is 20% of his basic salary. Write a program to calculate his gross salary.*/
+
 #include<stdio.h>
 int main(){
 double Basic_salary,Gross_salary,Rent,Dearness_allow;
