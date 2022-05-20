03412e53ce67a40ccb6c45af505c25a57d6e24ab
Nowshin <105603725+Nowshin1009@users.noreply.github.com>
2022-05-20 10:03:08 +0600

    Add files via upload

---
+++ Lab problem 8.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d,e,d1,d2,d3,d4,d5,sum,number;
+
+printf("Enter a five digit number : ");
+scanf("%d",&number);
+
+a=number/10;
+d5=number%10;
+b=a/10;
+d4=a%10;
+c=b/10;
+d3=b%10;
+d=c/10;
+d2=c%10;
+e=d/10;
+d1=d%10;
+
+printf("The reverce oder is  %d \t %d \t %d \t %d \t %d \n",d5,d4,d3,d2,d1);
+	
+	return 0;
+	
+	
+}
