1a0e202fb3b037c01e14106ba139fcdc1720d273
Nowshin <105603725+Nowshin1009@users.noreply.github.com>
2022-05-20 10:13:07 +0600

    Add files via upload

---
+++ Lab problem 6.c
@@ -0,0 +1,17 @@
+/*Two numbers are input through the keyboard into two locations C and D. Write a program to
+interchange the contents of C and D.  */
+
+#include<stdio.h>
+int main()
+{
+	int c,d;
+
+
+	printf("Enter the value of c:");
+	scanf("%d",&c);
+	printf("Enter the value of d:");
+	scanf("%d",&d);
+	printf("c=%d\nd=%d",d,c);
+
+	return 0;
+	}
