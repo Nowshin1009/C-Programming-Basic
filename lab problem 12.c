06f367a763dd088f4a92f10918b4f50516d192e7
Nowshin <105603725+Nowshin1009@users.noreply.github.com>
2022-05-20 22:19:27 +0600

    Add files via upload

---
+++ lab problem 12.c
@@ -0,0 +1,27 @@
+/*If the total selling price of 15 items and the total profit earned on them is input through the keyboard, write a program to find the cost price of one item.*/
+
+
+#include<stdio.h>
+int main()
+{
+	int selling_price,total_profit,cost_price;
+	double one_item;
+	
+	
+	printf("Enter the selling price of 15 items:");
+	scanf("%d",&selling_price);
+	printf("Enter the total profit of 15 items:");
+	scanf("%d",&total_profit);
+	
+	cost_price=selling_price-total_profit;
+	
+	one_item=cost_price/15;
+	
+     printf("Cost of one item:%.2lf",one_item);
+	
+	
+	return 0;
+	
+	
+	
+}
